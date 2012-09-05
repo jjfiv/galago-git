f6ae6d9b653f45f95834436f80ca04e6115af42a
Marc Cartright <irmarc@cs.umass.edu>
2012-09-05 16:27:32 -0400

    Updated the tests for a corrected BM25 scorer.

--- core/src/test/java/org/lemurproject/galago/core/scoring/BM25ScorerTest.java
+++ core/src/test/java/org/lemurproject/galago/core/scoring/BM25ScorerTest.java
@@ -35,8 +35,8 @@ public class BM25ScorerTest extends TestCase {
     assertEquals(0.75, scorer.b);
     assertEquals(1.2, scorer.k);
     assertEquals(50.0, scorer.avgDocLength);
-    assertEquals(5.30330, scorer.idf, 0.0001);
-    assertEquals(8.21639, scorer.score(5, 100), 0.0001);
+    assertEquals(5.29832, scorer.idf, 0.0001);
+    assertEquals(8.20866, scorer.score(5, 100), 0.0001);
 
     // Add in an iterator w/ some docs
     iterator = new FakeExtentIterator(five);
@@ -45,8 +45,8 @@ public class BM25ScorerTest extends TestCase {
     assertEquals(0.75, scorer.b);
     assertEquals(1.2, scorer.k);
     assertEquals(50.0, scorer.avgDocLength);
-    assertEquals(2.85438, scorer.idf, 0.0001);
-    assertEquals(5.44870, scorer.score(12, 85), 0.0001);
+    assertEquals(2.90042, scorer.idf, 0.0001);
+    assertEquals(5.53660, scorer.score(12, 85), 0.0001);
 
     // explicitly set everything
     p.set("b", 0.3);
@@ -56,7 +56,7 @@ public class BM25ScorerTest extends TestCase {
     assertEquals(0.3, scorer.b);
     assertEquals(2.0, scorer.k);
     assertEquals(50.0, scorer.avgDocLength);
-    assertEquals(1.36783, scorer.idf, 0.0001);
-    assertEquals(3.27407, scorer.score(15, 200), 0.0001);
+    assertEquals(1.58474, scorer.idf, 0.0001);
+    assertEquals(3.79327, scorer.score(15, 200), 0.0001);
   }
 }
