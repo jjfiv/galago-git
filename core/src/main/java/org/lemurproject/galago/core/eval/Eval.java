393d12adda6abeec15d22a397b6c7a932c2460ed
Unknown <sjh@compy-386.local>
2012-11-13 15:32:36 -0500

    Modified eval to use the CollectQueries function in BatchSearch

--- core/src/main/java/org/lemurproject/galago/core/eval/Eval.java
+++ core/src/main/java/org/lemurproject/galago/core/eval/Eval.java
@@ -9,23 +9,24 @@ import org.lemurproject.galago.core.eval.aggregate.QuerySetEvaluatorFactory;
 import org.lemurproject.galago.core.eval.compare.QuerySetComparator;
 import org.lemurproject.galago.core.eval.compare.QuerySetComparatorFactory;
 import org.lemurproject.galago.core.tools.AppFunction;
+import org.lemurproject.galago.core.tools.BatchSearch;
 import org.lemurproject.galago.tupleflow.Parameters;
 import org.lemurproject.galago.tupleflow.Parameters.Type;
 
 /**
- * Main method for retrieval evaluation.
- * Evaluates query results using a set of standard TREC metrics
- * 
- * 
+ * Main method for retrieval evaluation. Evaluates query results using a set of
+ * standard TREC metrics
+ *
+ *
  * @author trevor, sjh, jdalton
  */
 public class Eval extends AppFunction {
-  
+
   @Override
-  public String getName(){
+  public String getName() {
     return "eval";
   }
-  
+
   @Override
   public String getHelpString() {
     return "galago eval <parameters>+: \n"
@@ -84,7 +85,7 @@ public class Eval extends AppFunction {
             + "       h-randomized-0.01 Largest value of k such that the randomized test has a p-value of less than 0.1. \n"
             + "  The third column is the value of the test.\n";
   }
-  
+
   @Override
   public void run(Parameters p, PrintStream output) throws Exception {
     // check parameter's validity
@@ -97,25 +98,25 @@ public class Eval extends AppFunction {
     assert (!p.containsKey("metrics") || p.isList("metrics", Type.STRING)) : "eval parameter 'metrics' must be a list of strings.";
     assert (p.get("summary", true) || p.get("details", false)) : "eval requires either 'summary' or 'details' to be set true.";
     assert (!p.containsKey("comparisons") || p.isList("comparisons", Type.STRING)) : "eval parameter 'comparisons' must be a list of strings.";
-    
-    
+
+
     QuerySetResults baseline = new QuerySetResults(p.getString("baseline"));
-    QuerySetJudgments judgments = new QuerySetJudgments(p.getString("judgments"));    
+    QuerySetJudgments judgments = new QuerySetJudgments(p.getString("judgments"));
 
     // this ensure that queries that return no documents are represented in the ranking
     if (p.isList("queries", Type.MAP)) {
       List<Parameters> queries = (List<Parameters>) p.getList("queries");
       baseline.ensureQuerySet(queries);
     }
-    
+
     if (!p.containsKey("treatment")) {
       singleEvaluation(p, baseline, judgments, output);
     } else {
       QuerySetResults treatment = new QuerySetResults(p.getString("treatment"));
 
       // this ensure that queries that return no documents are represented in the ranking
-      if (p.isList("queries", Type.MAP)) {
-        List<Parameters> queries = (List<Parameters>) p.getList("queries");
+      List<Parameters> queries = BatchSearch.collectQueries(p);
+      if (!queries.isEmpty()) {
         treatment.ensureQuerySet(queries);
       }
 
@@ -124,24 +125,23 @@ public class Eval extends AppFunction {
   }
 
   /**
-   * When run as a standalone application, this returns output 
-   * very similar to that of trec_eval.  The first argument is 
-   * the ranking file, and the second argument is the judgments
-   * file, both in standard TREC format.
+   * When run as a standalone application, this returns output very similar to
+   * that of trec_eval. The first argument is the ranking file, and the second
+   * argument is the judgments file, both in standard TREC format.
    */
   public void singleEvaluation(Parameters p, QuerySetResults results, QuerySetJudgments judgments, PrintStream output) {
     String formatString = "%2$-16s%1$3s %3$6.5f\n";
     String[] metrics = new String[]{"num_ret", "num_rel", "num_rel_ret", "map",
-      "R-prec", "bpref", "recip_rank", "ndcg", "ndcg5", "ndcg10", "ndcg20", "ERR", "ERR10", "ERR20","P1",
+      "R-prec", "bpref", "recip_rank", "ndcg", "ndcg5", "ndcg10", "ndcg20", "ERR", "ERR10", "ERR20", "P1",
       "P5", "P10", "P15", "P20", "P30", "P100", "P200", "P500", "P1000"};
 
     // override default list if specified:
     if (p.containsKey("metrics")) {
       metrics = (String[]) p.getAsList("metrics").toArray(new String[0]);
     }
-    
+
     QuerySetEvaluator[] setEvaluators = createSetEvaluators(metrics, p);
-    
+
     if (p.get("details", false)) {
       for (String query : results.getQueryIterator()) {
         for (QuerySetEvaluator setEvaluator : setEvaluators) {
@@ -149,25 +149,25 @@ public class Eval extends AppFunction {
         }
       }
     }
-    
+
     if (p.get("summary", true)) {
       for (QuerySetEvaluator setEvaluator : setEvaluators) {
         output.format(formatString, "all", setEvaluator.getMetric(), setEvaluator.evaluate(results, judgments));
       }
     }
   }
-  
+
   public void comparisonEvaluation(Parameters p, QuerySetResults baseline, QuerySetResults treatment, QuerySetJudgments judgments,
           PrintStream output) {
     String formatString = "%1$-20s%2$-20s%3$6.4f\n";
-    
+
     String[] metrics = {"map", "R-prec", "bpref", "ndcg", "ndcg5", "ndcg10", "ndcg20", "P1", "P5", "P10", "P20"};
     // override default list if specified:
     if (p.containsKey("metrics")) {
       metrics = (String[]) p.getAsList("metrics").toArray(new String[0]);
     }
-    
-    
+
+
     String[] tests = new String[]{"baseline", "treatment", "baseBetter", "treatBetter", "equal", "ttest", "signtest", "randomized",
       "h-ttest-0.05", "h-signtest-0.05", "h-randomized-0.05", "h-ttest-0.01", "h-signtest-0.01", "h-randomized-0.01"};
 
@@ -175,21 +175,21 @@ public class Eval extends AppFunction {
     if (p.containsKey("comparisons")) {
       tests = (String[]) p.getAsList("comparisons").toArray(new String[0]);
     }
-    
+
     QuerySetEvaluator[] setEvaluators = createSetEvaluators(metrics, p);
     QuerySetComparator[] setComparators = createSetComparators(tests);
-    
+
     for (QuerySetEvaluator evaluator : setEvaluators) {
       String metricString = evaluator.getMetric();
       QuerySetEvaluation baseResults = evaluator.evaluateSet(baseline, judgments);
       QuerySetEvaluation treatResults = evaluator.evaluateSet(treatment, judgments);
-      
+
       for (QuerySetComparator comparator : setComparators) {
         output.format(formatString, metricString, comparator.getTestName(), comparator.evaluate(baseResults, treatResults));
       }
     }
   }
-  
+
   private QuerySetEvaluator[] createSetEvaluators(String[] metrics, Parameters p) {
     QuerySetEvaluator[] setEvaluators = new QuerySetEvaluator[metrics.length];
     for (int i = 0; i < metrics.length; i++) {
@@ -197,7 +197,7 @@ public class Eval extends AppFunction {
     }
     return setEvaluators;
   }
-  
+
   private QuerySetComparator[] createSetComparators(String[] comparasionMetrics) {
     QuerySetComparator[] setComparators = new QuerySetComparator[comparasionMetrics.length];
     for (int i = 0; i < comparasionMetrics.length; i++) {
