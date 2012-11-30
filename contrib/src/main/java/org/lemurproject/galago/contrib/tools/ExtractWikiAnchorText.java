21e5db5791e5359fc5af095257ee511544af1ea8
Marc Cartright <irmarc@cs.umass.edu>
2012-11-30 11:17:00 -0500

    Removed useless delayed processing models, moved the classes out of 'nasty' into the correct 'contrib' package.

--- contrib/src/main/java/org/lemurproject/galago/contrib/tools/ExtractWikiAnchorText.java
+++ contrib/src/main/java/org/lemurproject/galago/contrib/tools/ExtractWikiAnchorText.java
@@ -1,17 +1,20 @@
-package org.lemurproject.galago.core.tools;
+package org.lemurproject.galago.contrib.tools;
 
 import java.io.IOException;
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import org.lemurproject.galago.core.nasty.AnchorTextWriter;
+import org.lemurproject.galago.contrib.index.AnchorTextWriter;
 import org.lemurproject.galago.core.parse.AnchorTextCreator;
 import org.lemurproject.galago.core.parse.DocumentNumberer;
 import org.lemurproject.galago.core.parse.DocumentSource;
 import org.lemurproject.galago.core.parse.LinkCombiner;
 import org.lemurproject.galago.core.parse.LinkExtractor;
 import org.lemurproject.galago.core.parse.NumberedDocumentDataExtractor;
+import org.lemurproject.galago.core.tools.AppFunction;
+import org.lemurproject.galago.core.tools.BuildIndex;
+import org.lemurproject.galago.core.tools.BuildStageTemplates;
 import org.lemurproject.galago.core.types.AdditionalDocumentText;
 import org.lemurproject.galago.core.types.DocumentSplit;
 import org.lemurproject.galago.core.types.ExtractedLink;
