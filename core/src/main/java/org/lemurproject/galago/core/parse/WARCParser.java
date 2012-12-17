d9fabef8199dc47886cc8f48800f6a5280bc13bd
Unknown <sjh@benalla>
2012-12-17 11:17:34 -0500

    Updated WARC parser to match CluewebUtils

--- core/src/main/java/org/lemurproject/galago/core/parse/WARCParser.java
+++ core/src/main/java/org/lemurproject/galago/core/parse/WARCParser.java
@@ -11,7 +11,10 @@
 package org.lemurproject.galago.core.parse;
 
 import java.io.DataInputStream;
+import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map.Entry;
 import org.lemurproject.galago.core.types.DocumentSplit;
 import org.lemurproject.galago.tupleflow.Parameters;
 
@@ -45,10 +48,41 @@ public class WARCParser extends DocumentStreamParser {
 
     totalNumBytesRead += (long) record.getTotalRecordLength();
 
-    Document doc = new Document(record.getDocid(), record.getContent());
-    doc.metadata = record.warcHeader.metadata;
+    Document doc = new Document(record.getHeaderMetadataItem("WARC-TREC-ID"), record.getContentUTF8());
+    doc.metadata = new HashMap();
+    for(Entry<String, String> entry : record.getHeaderMetadata()){
+      doc.metadata.put(entry.getKey(), entry.getValue());
+    }
     doc.metadata.put("url", record.getHeaderMetadataItem("WARC-Target-URI"));
 
     return doc;
+  }
+  
+  /**
+   * Test function for parser.
+   */
+  public static void main(String[] args) throws IOException{
+    if(args.length < 1){
+      System.out.println("Usage: <filename.warc>");
+      return;
+    }
+    
+    File f = new File(args[0]);
+    if(!f.isFile()){
+      System.out.println("File does not exist");
+      System.out.println("Usage: <filename.warc>");
+      return;
+    }
+    
+    WARCParser parser = new WARCParser(new DocumentSplit(f.getAbsolutePath(), "warc", false, new byte[0], new byte[0], 1, 1), new Parameters());
+    Document d;
+    while((d = parser.nextDocument()) != null){
+      System.out.format( "NAME-:\n%s\n---\n", d.name );
+      System.out.format( "META-DATA-:\n");
+      for(String key : d.metadata.keySet()){
+        System.out.format( "%s -: %s\n", key, d.metadata.get(key));
+      }
+      System.out.format( "TEXT-:\n%s\n---\n", d.text );
+    }
   }
 }
