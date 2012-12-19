a7174f16e05d1559d2f2cd7bf88e0d3c8e47569e
Unknown <sjh@compy-386.local>
2012-12-18 19:17:58 -0500

    Bug fix

--- contrib/src/main/java/org/lemurproject/galago/contrib/parse/WordCountCleaner.java
+++ contrib/src/main/java/org/lemurproject/galago/contrib/parse/WordCountCleaner.java
@@ -23,20 +23,20 @@ import org.lemurproject.galago.tupleflow.execution.Verified;
 @InputClass(className = "org.lemurproject.galago.core.types.WordCount")
 @OutputClass(className = "org.lemurproject.galago.core.types.WordCount")
 public class WordCountCleaner extends StandardStep<WordCount, WordCount> {
-  
-  HashMap<String,String> replacements;
+
+  HashMap<String, String> replacements;
   String exceptions;
   boolean stripPunct;
   boolean lower;
-  
+
   public WordCountCleaner(TupleFlowParameters p) throws Exception {
     stripPunct = p.getJSON().get("stripPunct", true);
-    lower = p.getJSON().get("lower", true);    
+    lower = p.getJSON().get("lower", true);
 
     Parameters repls = p.getJSON().getMap("replacements");
     replacements = new HashMap();
     exceptions = "[^\\w";
-    for(String key: repls.getKeys()){
+    for (String key : repls.getKeys()) {
       replacements.put(key, repls.getString(key));
       exceptions += key;
     }
@@ -46,20 +46,23 @@ public class WordCountCleaner extends StandardStep<WordCount, WordCount> {
   @Override
   public void process(WordCount wc) throws IOException {
     String t = Utility.toString(wc.word);
-    if(lower){
+    if (lower) {
       t = t.toLowerCase();
     }
-    if(stripPunct){
+    if (stripPunct) {
       // remove all punctuation
       // NOTE: java 1.7 allows \W to match unicode chars correctly.
       t = Pattern.compile(exceptions).matcher(t).replaceAll("");
     }
-    
-    for(String key : replacements.keySet()){
+
+    for (String key : replacements.keySet()) {
       t = t.replaceAll(key, replacements.get(key));
     }
-    
-    wc.word = Utility.fromString(t);
-    processor.process(wc);
+
+    // empty strings should be removed
+    if (!t.isEmpty()) {
+      wc.word = Utility.fromString(t);
+      processor.process(wc);
+    }
   }
 }
