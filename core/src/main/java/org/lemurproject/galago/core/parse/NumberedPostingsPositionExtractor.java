dd6dd847d08ed89db9025288929fb4d000674fcc
David Fisher <david_fisher@users.sourceforge.net>
2013-07-11 15:07:56 -0400

    BUG#214 -- remove null pointer exception case and ## space insertion.

--- core/src/main/java/org/lemurproject/galago/core/parse/NumberedPostingsPositionExtractor.java
+++ core/src/main/java/org/lemurproject/galago/core/parse/NumberedPostingsPositionExtractor.java
@@ -11,29 +11,22 @@ import org.lemurproject.galago.tupleflow.execution.Verified;
 
 /**
  *
- * @author trevor
+ * @author trevor, dmf
  */
 @Verified
 @InputClass(className = "org.lemurproject.galago.core.parse.Document")
 @OutputClass(className = "org.lemurproject.galago.core.types.NumberWordPosition")
 public class NumberedPostingsPositionExtractor extends StandardStep<Document, NumberWordPosition> {
-  public static final int SPACING = 10;
-  public int spaces;
+
+  @Override
   public void process(Document object) throws IOException {
-    spaces = 0;
     for (int i = 0; i < object.terms.size(); i++) {
       String term = object.terms.get(i);
-      if (term.equals("##")) {
-        ++spaces;
-        continue;
-      }
       if (term == null) {
         continue;
       }
 
-      processor.process(new NumberWordPosition(object.identifier,
-                Utility.fromString(term),
-                i+(spaces*SPACING)));
+      processor.process(new NumberWordPosition(object.identifier, Utility.fromString(term), i));
     }
   }
 }
