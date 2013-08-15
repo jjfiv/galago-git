e6b0db31cc86d6c2f163e693cd7c1408e67cc92f
David Fisher <david_fisher@users.sourceforge.net>
2013-08-15 10:48:02 -0400

    Clear out extraneous comments.

--- core/src/test/java/org/lemurproject/galago/core/retrieval/traversal/RelevanceFeedbackTraversalTest.java
+++ core/src/test/java/org/lemurproject/galago/core/retrieval/traversal/RelevanceFeedbackTraversalTest.java
@@ -23,7 +23,7 @@ import org.lemurproject.galago.tupleflow.Utility;
  *
  * TODO: Make stronger tests to increase confidence
  *
- * @author irmarc, sjh, dietz
+ * @author irmarc, sjh, dietz, dmf
  */
 public class RelevanceFeedbackTraversalTest extends TestCase {
 
@@ -47,28 +47,6 @@ public class RelevanceFeedbackTraversalTest extends TestCase {
     indexFile = files[2];
   }
 
-//  public void testRelevanceModelTraversal() throws Exception {
-//    // Create a retrieval object for use by the traversal
-//    Parameters p = new Parameters();
-//    p.set("index", indexFile.getAbsolutePath());
-//    p.set("stemmedPostings", false);
-//    p.set("fbOrigWeight", 0.5);
-//
-//    LocalRetrieval retrieval = (LocalRetrieval) RetrievalFactory.instance(p);
-//    RelevanceModelTraversal traversal = new RelevanceModelTraversal(retrieval);
-//
-//    Node parsedTree = StructuredQuery.parse("#rm:fbTerms=3:fbDocs=2( #dirichlet( #extents:fits:part=postings() ) )");
-//    Node transformed = StructuredQuery.copy(traversal, parsedTree, new Parameters());
-//    // truth data
-//    StringBuilder correct = new StringBuilder();
-//    correct.append("#combine:0=0.5:1=0.5( ");
-//    correct.append("#combine:fbDocs=2:fbTerms=3( #dirichlet( #extents:fits:part=postings() ) ) ");
-//    correct.append("#combine:0=0.12516622340425526:1=0.04161125886524822:2=0.04161125886524822( #text:program() #text:shoe() #text:ugly() ) )");
-//
-//    assertEquals(correct.toString(), transformed.toString());
-//    retrieval.close();
-//  }
-
   public void testRelevanceModel1Traversal() throws Exception {
     // Create a retrieval object for use by the traversal
     Parameters p = new Parameters();
@@ -84,7 +62,6 @@ public class RelevanceFeedbackTraversalTest extends TestCase {
     Node transformed = StructuredQuery.copy(traversal, parsedTree, new Parameters());
     // truth data
     StringBuilder correct = new StringBuilder();
-//    correct.append("#combine:0=0.5:1=0.5( #combine:fbDocs=10:fbTerms=4( #dirichlet( #extents:jumped:part=postings() ) ) ");
     /* No sentiwordlist.txt
     correct.append("#combine:0=0.05001660577881102:1=0.05001660577881102:2=0.04165282851765748:3=0.04165282851765748( ");
     correct.append("#text:sample() ");
@@ -92,7 +69,7 @@ public class RelevanceFeedbackTraversalTest extends TestCase {
     correct.append("#text:cat() ");
     correct.append("#text:moon() )");
   */
-       correct.append("#combine:0=0.05001660577881102:1=0.04165282851765748( ");
+    correct.append("#combine:0=0.05001660577881102:1=0.04165282851765748( ");
     correct.append("#text:ugly() ");
     correct.append("#text:moon() )");
 
@@ -109,7 +86,7 @@ public class RelevanceFeedbackTraversalTest extends TestCase {
     Parameters p = new Parameters();
     p.set("index", indexFile.getAbsolutePath());
     p.set("stemmedPostings", false);
-    p.set("fbOrigWeight", 0.5);
+    p.set("fbOrigWeight", 0.9);
     p.set("relevanceModel", RelevanceModel3.class.getName());
     p.set("rmwhitelist", "sentiwordlist.txt");
     LocalRetrieval retrieval = (LocalRetrieval) RetrievalFactory.instance(p);
@@ -119,67 +96,18 @@ public class RelevanceFeedbackTraversalTest extends TestCase {
     Node transformed = StructuredQuery.copy(traversal, parsedTree, new Parameters());
     // truth data
     StringBuilder correct = new StringBuilder();
-//    correct.append("#combine:0=0.5:1=0.5( #combine:fbDocs=10:fbTerms=4( #dirichlet( #extents:jumped:part=postings() ) ) ");
-    /* No sentiwordlist.txt
-    correct.append("#combine:0=0.05001660577881102:1=0.05001660577881102:2=0.04165282851765748:3=0.04165282851765748( ");
-    correct.append("#text:sample() ");
-    correct.append("#text:ugly() ");
-    correct.append("#text:cat() ");
-    correct.append("#text:moon() )");
-  */
-       correct.append("#combine:0=0.5:1=0.5( #combine:fbDocs=10:fbTerms=4( #dirichlet( #extents:jumped:part=postings() ) ) ");
+
+    correct.append("#combine:0=0.9:1=0.09999999999999998( #combine:fbDocs=10:fbTerms=4( #dirichlet( #extents:jumped:part=postings() ) ) ");
     correct.append("#combine:0=0.05001660577881102:1=0.04165282851765748( #text:ugly() #text:moon() ) )");
     
+    //System.err.println(transformed.toString());
+    //System.err.println(correct.toString());
+
     assertEquals(correct.toString(), transformed.toString());
-    
+ 
     retrieval.close();
   }
    
-//  public void testClassloaderRelevanceModelTraversal() throws Exception {
-//    // Create a retrieval object for use by the traversal
-//    Parameters p = new Parameters();
-//    p.set("index", indexFile.getAbsolutePath());
-//    p.set("corpus", corpusFile.getAbsolutePath());
-//    p.set("stemmedPostings", false);
-//    p.set("fbOrigWeight", 0.5);
-//    p.set("relevanceModel", "org.lemurproject.galago.core.retrieval.prf.RelevanceModel3");
-//    
-//    LocalRetrieval retrieval = (LocalRetrieval) RetrievalFactory.instance(p);
-//    RelevanceModelTraversal traversal = new RelevanceModelTraversal(retrieval);
-//
-//    Node parsedTree = StructuredQuery.parse("#rm:fbTerms=3:fbDocs=10( #dirichlet( #extents:jumped:part=postings() ) )");
-//    Node transformed = StructuredQuery.copy(traversal, parsedTree, new Parameters());
-//
-//    // truth data
-//    StringBuilder correct = new StringBuilder();
-//    correct.append("#combine:0=0.5:1=0.5( #combine:fbDocs=10:fbTerms=3( #dirichlet( #extents:jumped:part=postings() ) ) ");
-//    correct.append("#combine:0=0.05001660577881102:1=0.05001660577881102:2=0.04165282851765748:3=0.04165282851765748( #text:sample() #text:ugly() #text:cat() #text:moon() ) )");
-//
-//    assertEquals(correct.toString(), transformed.toString());
-//    retrieval.close();
-//  }
-//
-////  public void testBM25RelevanceFeedbackTraversal() throws Exception {
-////    // Create a retrieval object for use by the traversal
-////    Parameters p = new Parameters();
-////    p.set("retrievalGroup", "all");
-////    p.set("index", indexFile.getAbsolutePath());
-////    p.set("corpus", corpusFile.getAbsolutePath());
-////    LocalRetrieval retrieval = (LocalRetrieval) RetrievalFactory.instance(p);
-////    BM25RelevanceFeedbackTraversal traversal = new BM25RelevanceFeedbackTraversal(retrieval);
-////    Node parsedTree = StructuredQuery.parse("#bm25rf:fbDocs=3:fbTerms=2( #bm25( #extents:cat:part=postings() ) )");
-////    Node transformed = StructuredQuery.copy(traversal, parsedTree);
-////    //truth data
-////    StringBuilder correct = new StringBuilder();
-////    correct.append("#combine( #bm25( #extents:cat:part=postings() ) ");
-////    correct.append("#bm25rf:R=3:rt=1( #extents:jumped:part=postings() ) ");
-////    correct.append("#bm25rf:R=3:rt=2( #extents:moon:part=postings() ) )");
-////
-////    assertEquals(correct.toString(), transformed.toString());
-////
-////    retrieval.close();
-////  }
-
   @Override
   public void tearDown() throws Exception {
     if (relsFile != null) {
