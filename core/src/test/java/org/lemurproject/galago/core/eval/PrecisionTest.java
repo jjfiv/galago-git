aadf7b8275c8a196ef2d799f8a67f825222cf23f
Unknown <wem@mildura.cs.umass.edu>
2014-03-20 15:09:04 -0400

    Recall Test completed

--- core/src/test/java/org/lemurproject/galago/core/eval/PrecisionTest.java
+++ core/src/test/java/org/lemurproject/galago/core/eval/PrecisionTest.java
@@ -14,11 +14,6 @@ import org.lemurproject.galago.tupleflow.Utility;
 
 /**
  * Test the precision class
- * Checks the following calculations:
- * Precision when all retrieved documents are relevant
- * Precision when some retrieved documents are relevant
- * Precision when some documents are not scored for a query
- * Precision when no retrieved documents are relevant
  * @author wem
  */
 @RunWith(JUnit4.class)
@@ -29,47 +24,79 @@ public class PrecisionTest {
         File tmpR = FileUtility.createTemporary();
 
     try{
+        //there are 10 documents total
+        //query 1 has 10 relevant documents
+        //query 2 has 5 relevant documents
+        //query 3 has 5 relevant documents
         String qrels =
                 "1 0 doc0 1\n" +
                 "1 0 doc1 1\n" +
                 "1 0 doc2 1\n" +
                 "1 0 doc3 1\n" +
+                "1 0 doc4 1\n" +
                 "2 0 doc0 0\n" +
-                "2 0 doc1 1\n" +
-                "2 0 doc2 1\n" +
-                "2 0 doc3 1\n" +
+                "2 0 doc1 0\n" +
+                "2 0 doc2 0\n" +
+                "2 0 doc3 0\n" +
+                "2 0 doc4 0\n" +
+                "2 0 doc5 1\n" +
+                "2 0 doc6 1\n" +
+                "2 0 doc7 1\n" +
+                "2 0 doc8 1\n" +
+                "2 0 doc9 1\n" +
+                "3 0 doc0 1\n" +
                 "3 0 doc1 1\n" +
                 "3 0 doc2 1\n" +
                 "3 0 doc3 1\n" +
-                "4 0 doc1 0\n";
-
+                "3 0 doc4 1\n" +
+                "3 0 doc5 0\n" +
+                "3 0 doc6 0\n" +
+                "3 0 doc7 0\n" +
+                "3 0 doc8 0\n" +
+                "3 0 doc9 0\n";
         Utility.copyStringToFile(qrels, tmpQ);
         QuerySetJudgments qsj = new QuerySetJudgments(tmpQ.getAbsolutePath(), true, true);
         String results =
-                "1 Q0 doc1 1 -12.08 galago\n" +
-                "1 Q0 doc2 2 -12.12 galago\n" +
-                "1 Q0 doc3 3 -13.08 galago\n" +
-                "1 Q0 doc0 4 -13.55 galago\n" +
-                "2 Q0 doc1 1 -12.08 galago\n" +
-                "2 Q0 doc2 2 -12.12 galago\n" +
-                "2 Q0 doc3 3 -13.08 galago\n" +
-                "2 Q0 doc0 4 -13.55 galago\n" +
-                "3 Q0 doc1 1 -12.08 galago\n" +
-                "3 Q0 doc2 2 -12.12 galago\n" +
-                "3 Q0 doc3 3 -13.08 galago\n" +
-                "3 Q0 doc0 4 -13.55 galago\n" +
-                "4 Q0 doc1 1 -12.08 galago\n" +
-                "4 Q0 doc2 2 -12.12 galago\n" +
-                "4 Q0 doc3 3 -13.08 galago\n" +
-                "4 Q0 doc0 4 -13.55 galago\n";
+                "1 Q0 doc0 1 -10.08 galago\n" +
+                "1 Q0 doc1 2 -10.12 galago\n" +
+                "1 Q0 doc2 3 -11.08 galago\n" +
+                "1 Q0 doc3 4 -11.55 galago\n" +
+                "1 Q0 doc4 5 -11.85 galago\n" +
+                "1 Q0 doc5 6 -12.08 galago\n" +
+                "1 Q0 doc6 7 -12.12 galago\n" +
+                "1 Q0 doc7 8 -13.08 galago\n" +
+                "1 Q0 doc8 9 -13.55 galago\n" +
+                "1 Q0 doc9 10 -13.85 galago\n" +
+                "2 Q0 doc0 1 -10.08 galago\n" +
+                "2 Q0 doc1 2 -10.12 galago\n" +
+                "2 Q0 doc2 3 -11.08 galago\n" +
+                "2 Q0 doc3 4 -11.55 galago\n" +
+                "2 Q0 doc4 5 -11.85 galago\n" +
+                "2 Q0 doc5 6 -12.08 galago\n" +
+                "2 Q0 doc6 7 -12.12 galago\n" +
+                "2 Q0 doc7 8 -13.08 galago\n" +
+                "2 Q0 doc8 9 -13.55 galago\n" +
+                "2 Q0 doc9 10 -13.85 galago\n" +
+                "3 Q0 doc0 1 -10.08 galago\n" +
+                "3 Q0 doc1 2 -10.12 galago\n" +
+                "3 Q0 doc2 3 -11.08 galago\n" +
+                "3 Q0 doc3 4 -11.55 galago\n" +
+                "3 Q0 doc4 5 -11.85 galago\n" +
+                "3 Q0 doc5 6 -12.08 galago\n" +
+                "3 Q0 doc6 7 -12.12 galago\n" +
+                "3 Q0 doc7 8 -13.08 galago\n" +
+                "3 Q0 doc8 9 -13.55 galago\n" +
+                "3 Q0 doc9 10 -13.85 galago\n";
         Utility.copyStringToFile(results, tmpR);
         QuerySetResults qsr = new QuerySetResults(tmpR.getAbsolutePath());
-        Precision p = new Precision(4);
-        assertEquals(1.00, p.evaluate(qsr.get("1"), qsj.get("1")),.0001);
-        assertEquals(0.75, p.evaluate(qsr.get("2"), qsj.get("2")),.0001);
-        assertEquals(0.75, p.evaluate(qsr.get("3"), qsj.get("3")),.0001);
-        assertEquals(0.00, p.evaluate(qsr.get("4"), qsj.get("4")),.0001);
-        assertEquals(1.00, (new Precision(3)).evaluate(qsr.get("2"), qsj.get("2")),.0001);
+        Precision pFive = new Precision(5);
+        Precision pTen = new Precision(10);
+        assertEquals(1.00, pFive.evaluate(qsr.get("1"), qsj.get("1")),.0001);
+        assertEquals(0.00, pFive.evaluate(qsr.get("2"), qsj.get("2")),.0001);
+        assertEquals(1.00, pFive.evaluate(qsr.get("3"), qsj.get("3")),.0001);
+        assertEquals(0.50, pTen.evaluate(qsr.get("1"), qsj.get("1")),.0001);
+        assertEquals(0.50, pTen.evaluate(qsr.get("2"), qsj.get("2")),.0001);
+        assertEquals(0.50, pTen.evaluate(qsr.get("3"), qsj.get("3")),.0001);
     }
         finally{
             tmpQ.delete();
