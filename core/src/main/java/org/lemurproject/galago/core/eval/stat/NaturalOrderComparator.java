79b3d40999c8a4174594225840e0f49a06b781ed
John Foley <jfoley@cs.umass.edu>
2014-03-28 13:15:05 -0400

    Remove mostly-unused code from Stat package.

--- core/src/main/java/org/lemurproject/galago/core/eval/stat/NaturalOrderComparator.java
+++ core/src/main/java/org/lemurproject/galago/core/eval/stat/NaturalOrderComparator.java
@@ -23,16 +23,14 @@ misrepresented as being the original software.
  */
 package org.lemurproject.galago.core.eval.stat;
 
-import java.util.Arrays;
-import java.util.Collections;
+import java.io.Serializable;
 import java.util.Comparator;
-import java.util.List;
 
 /**
  * Natural comparator is useful for sorting query ids (in eval)
  *  into a reasonable order.
  */
-public class NaturalOrderComparator implements Comparator {
+public class NaturalOrderComparator implements Comparator, Serializable {
 
   int compareRight(String a, String b) {
     int bias = 0;
@@ -136,25 +134,5 @@ public class NaturalOrderComparator implements Comparator {
     } else {
       return s.charAt(i);
     }
-  }
-
-  public static void main(String[] args) {
-    String[] strings = new String[]{"1-2", "1-02", "1-20", "10-20", "fred", "jane", "pic01",
-      "pic2", "pic02", "pic02a", "pic3", "pic4", "pic 4 else", "pic 5", "pic05", "pic 5",
-      "pic 5 something", "pic 6", "pic   7", "pic100", "pic100a", "pic120", "pic121",
-      "pic02000", "tom", "x2-g8", "x2-y7", "x2-y08", "x8-y8"};
-
-    List orig = Arrays.asList(strings);
-
-    System.out.println("Original: " + orig);
-
-    List scrambled = Arrays.asList(strings);
-    Collections.shuffle(scrambled);
-
-    System.out.println("Scrambled: " + scrambled);
-
-    Collections.sort(scrambled, new NaturalOrderComparator());
-
-    System.out.println("Sorted: " + scrambled);
   }
 }
---
+++ core/src/test/java/org/lemurproject/galago/core/eval/stat/NaturalOrderComparatorTest.java
@@ -0,0 +1,33 @@
+package org.lemurproject.galago.core.eval.stat;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author jfoley.
+ */
+public class NaturalOrderComparatorTest {
+  @Test
+  public void testNatOrderCmpTest() {
+    String[] strings = new String[]{"1-2", "1-02", "1-20", "10-20", "fred", "jane", "pic01",
+        "pic2", "pic02", "pic02a", "pic3", "pic4", "pic 4 else", "pic 5", "pic05", "pic 5",
+        "pic 5 something", "pic 6", "pic   7", "pic100", "pic100a", "pic120", "pic121",
+        "pic02000", "tom", "x2-g8", "x2-y7", "x2-y08", "x8-y8"};
+
+    List orig = Arrays.asList(strings);
+
+    System.out.println("Original: " + orig);
+
+    List scrambled = Arrays.asList(strings);
+    Collections.shuffle(scrambled);
+
+    System.out.println("Scrambled: " + scrambled);
+
+    Collections.sort(scrambled, new NaturalOrderComparator());
+
+    System.out.println("Sorted: " + scrambled);
+  }
+}
