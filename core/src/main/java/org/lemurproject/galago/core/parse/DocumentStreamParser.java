a69559fc842163eea1da1ad6c723a8a0ccb76678
John Foley <jfoley@cs.umass.edu>
2014-04-16 22:00:16 -0400

    Decouple UniversalParser and DocumentStreamParser; DocumentStreamParser owns a map of its subclasses now.

--- core/src/main/java/org/lemurproject/galago/core/parse/DocumentStreamParser.java
+++ core/src/main/java/org/lemurproject/galago/core/parse/DocumentStreamParser.java
@@ -8,6 +8,9 @@ import org.lemurproject.galago.tupleflow.StreamCreator;
 import org.tukaani.xz.XZInputStream;
 
 import java.io.*;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
 import java.util.zip.GZIPInputStream;
 
 /**
@@ -16,10 +19,88 @@ import java.util.zip.GZIPInputStream;
  */
 public abstract class DocumentStreamParser {
 
+  /* Static interface */
+
+  // The built-in type map
+  public static HashMap<String, Class> fileTypeMap;
+  static {
+    fileTypeMap = new HashMap<String, Class>();
+    fileTypeMap.put("html", FileParser.class);
+    fileTypeMap.put("xml", FileParser.class);
+    fileTypeMap.put("txt", FileParser.class);
+    fileTypeMap.put("arc", ArcParser.class);
+    fileTypeMap.put("warc", WARCParser.class);
+    fileTypeMap.put("trectext", TrecTextParser.class);
+    fileTypeMap.put("trecweb", TrecWebParser.class);
+    fileTypeMap.put("twitter", TwitterParser.class);
+    fileTypeMap.put("corpus", CorpusSplitParser.class);
+    fileTypeMap.put("selectivecorpus", CorpusSelectiveSplitParser.class);
+    fileTypeMap.put("wiki", WikiParser.class);
+  }
+
+  // isParsable
+  public static boolean hasParserForExtension(String ext) {
+    return fileTypeMap.containsKey(ext);
+  }
+
+  // configuring extra parsers
+  public static void addExternalParsers(Parameters parameters) {
+    try {
+      // Look for external mapping definitions
+      if (parameters.containsKey("externalParsers")) {
+        for (Parameters extP : parameters.getAsList("externalParsers", Parameters.class)) {
+          fileTypeMap.put(extP.getString("filetype"), Class.forName(extP.getString("class")));
+        }
+      }
+    } catch (ClassNotFoundException cnfe) {
+      throw new IllegalArgumentException(cnfe);
+    }
+  }
+
+  // create a parser
+  public static DocumentStreamParser instance(DocumentSplit split, Parameters parameters) throws IOException {
+    // Determine the file type either from the parameters
+    // or from the guess in the splits
+    String fileType;
+    if (parameters.containsKey("filetype")) {
+      fileType = parameters.getString("filetype");
+    } else {
+      fileType = split.fileType;
+    }
+
+    if (fileTypeMap.containsKey(fileType)) {
+      return constructParserWithSplit(fileTypeMap.get(fileType), split, parameters);
+    } else {
+      throw new IOException("Unknown fileType: " + fileType + " for fileName: " + split.fileName);
+    }
+  }
+
+  private static DocumentStreamParser constructParserWithSplit(Class parserClass, DocumentSplit split, Parameters parameters) throws IOException {
+    for(Constructor cons : parserClass.getConstructors()) {
+      Class<?>[] formals = cons.getParameterTypes();
+      if(formals.length != 2) continue;
+      if(formals[0].isAssignableFrom(DocumentSplit.class) && formals[1].isAssignableFrom(Parameters.class)) {
+        try {
+          return (DocumentStreamParser) cons.newInstance(split, parameters);
+        } catch (InstantiationException e) {
+          throw new IOException(e);
+        } catch (IllegalAccessException e) {
+          throw new IOException(e);
+        } catch (InvocationTargetException e) {
+          throw new IOException(e);
+        }
+      }
+    }
+    // None of the constructors worked. Complain.
+    throw new IllegalArgumentException("No viable constructor for file type parser " + parserClass.getName() +  "\n\n" +
+        "Expected (DocumentSplit split, Parameters p) in the constructor.\n");
+  }
+
+
   /** This is the constructor expected by UniversalParser
    *  It must be implemented in each implementing class
    */
-  public DocumentStreamParser(DocumentSplit split, Parameters p) {}
+  public DocumentStreamParser(DocumentSplit split, Parameters p) { }
 
   public abstract Document nextDocument() throws IOException;
 
--- core/src/main/java/org/lemurproject/galago/core/parse/UniversalParser.java
+++ core/src/main/java/org/lemurproject/galago/core/parse/UniversalParser.java
@@ -5,12 +5,7 @@ import org.lemurproject.galago.core.types.DocumentSplit;
 import org.lemurproject.galago.tupleflow.*;
 import org.lemurproject.galago.tupleflow.execution.Verified;
 
-import java.io.EOFException;
 import java.io.IOException;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.util.HashMap;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -29,21 +24,6 @@ import java.util.logging.Logger;
 @OutputClass(className = "org.lemurproject.galago.core.parse.Document")
 public class UniversalParser extends StandardStep<DocumentSplit, Document> {
 
-  // The built-in type map
-  static String[][] sFileTypeLookup = {
-    {"html", FileParser.class.getName()},
-    {"xml", FileParser.class.getName()},
-    {"txt", FileParser.class.getName()},
-    {"arc", ArcParser.class.getName()},
-    {"warc", WARCParser.class.getName()},
-    {"trectext", TrecTextParser.class.getName()},
-    {"trecweb", TrecWebParser.class.getName()},
-    {"twitter", TwitterParser.class.getName()},
-    {"corpus", CorpusSplitParser.class.getName()},
-    {"selectivecorpus", CorpusSelectiveSplitParser.class.getName()},
-    {"wiki", WikiParser.class.getName()}
-  };
-  private HashMap<String, Class> fileTypeMap;
   private Counter documentCounter;
   private Parameters parameters;
   private static final Logger LOG = Logger.getLogger(UniversalParser.class.getSimpleName());
@@ -52,27 +32,7 @@ public class UniversalParser extends StandardStep<DocumentSplit, Document> {
   public UniversalParser(TupleFlowParameters parameters) {
     documentCounter = parameters.getCounter("Documents Parsed");
     this.parameters = parameters.getJSON();
-    buildFileTypeMap();
-  }
-
-  private void buildFileTypeMap() {
-    try {
-      fileTypeMap = new HashMap<String, Class>();
-      for (String[] mapping : sFileTypeLookup) {
-        fileTypeMap.put(mapping[0], Class.forName(mapping[1]));
-      }
-
-      // Look for external mapping definitions
-      if (parameters.containsKey("externalParsers")) {
-        List<Parameters> externalParsers = parameters.getAsList("externalParsers", Parameters.class);
-        for (Parameters extP : externalParsers) {
-          fileTypeMap.put(extP.getString("filetype"),
-                  Class.forName(extP.getString("class")));
-        }
-      }
-    } catch (ClassNotFoundException cnfe) {
-      throw new IllegalArgumentException(cnfe);
-    }
+    DocumentStreamParser.addExternalParsers(this.parameters);
   }
 
   @Override
@@ -80,7 +40,6 @@ public class UniversalParser extends StandardStep<DocumentSplit, Document> {
 
     LOG.info("Processing split: "+split.fileName);
 
-    DocumentStreamParser parser = null;
     long count = 0;
     long limit = Long.MAX_VALUE;
     if (split.startKey.length > 0) {
@@ -89,31 +48,7 @@ public class UniversalParser extends StandardStep<DocumentSplit, Document> {
       }
     }
 
-    // Determine the file type either from the parameters
-    // or from the guess in the splits
-    String fileType;
-    if (parameters.containsKey("filetype")) {
-      fileType = parameters.getString("filetype");
-    } else {
-      fileType = split.fileType;
-    }
-
-    if (fileTypeMap.containsKey(fileType)) {
-      try {
-        parser = constructParserWithSplit(fileTypeMap.get(fileType), split, parameters);
-        assert(parser != null);
-      } catch (EOFException ee) {
-        System.err.printf("Found empty split %s. Skipping due to no content.", split.toString());
-        return;
-      } catch (Exception e) {
-        throw new RuntimeException(e);
-      } finally {
-        if(parser != null) parser.close();
-      }
-    } else {
-      throw new IOException("Unknown fileType: " + fileType
-              + " for fileName: " + split.fileName);
-    }
+    DocumentStreamParser parser = DocumentStreamParser.instance(split, parameters);
 
     // A parser is instantiated. Start producing documents for consumption
     // downstream.
@@ -150,34 +85,7 @@ public class UniversalParser extends StandardStep<DocumentSplit, Document> {
     }
   }
 
-  private static DocumentStreamParser constructParserWithSplit(Class parserClass, DocumentSplit split, Parameters parameters) throws IOException {
-    for(Constructor cons : parserClass.getConstructors()) {
-      Class<?>[] formals = cons.getParameterTypes();
-      if(formals.length != 2) continue;
-      if(formals[0].isAssignableFrom(DocumentSplit.class) && formals[1].isAssignableFrom(Parameters.class)) {
-        try {
-          return (DocumentStreamParser) cons.newInstance(split, parameters);
-        } catch (InstantiationException e) {
-          throw new IOException(e);
-        } catch (IllegalAccessException e) {
-          throw new IOException(e);
-        } catch (InvocationTargetException e) {
-          throw new IOException(e);
-        }
-      }
-    }
-    // None of the constructors worked. Complain.
-    throw new IllegalArgumentException("No viable constructor for file type parser " + parserClass.getName() +  "\n\n" +
-        "Expected (DocumentSplit split, Parameters p) in the constructor.\n");
-  }
-
-  /** returns true if internally defined */
   public static boolean isParsable(String extension) {
-    for (String[] entry : sFileTypeLookup) {
-      if (entry[0].equals(extension)) {
-        return true;
-      }
-    }
-    return false;
+    return DocumentStreamParser.hasParserForExtension(extension);
   }
 }
