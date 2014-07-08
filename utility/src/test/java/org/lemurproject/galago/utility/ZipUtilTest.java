98668b60e7967e28f8a42879c946f9adf0c42a39
michaelz <unknown>
2014-07-08 12:31:50 -0400

    Updated tests to remove assertTrue on file delete() so the tests pass on Windows.

--- utility/src/test/java/org/lemurproject/galago/utility/ZipUtilTest.java
+++ utility/src/test/java/org/lemurproject/galago/utility/ZipUtilTest.java
@@ -17,41 +17,43 @@ import static org.junit.Assert.assertTrue;
 
 public class ZipUtilTest {
 
-  @Test
-  public void testZipFile() throws IOException {
-    File tmp = null;
+    @Test
+    public void testZipFile() throws IOException {
+        File tmp = null;
 
-    try {
-      tmp = File.createTempFile("zipUtilTest", ".zip");
+        try {
+            tmp = File.createTempFile("zipUtilTest", ".zip");
 
-      String fooContents = "foo is the best";
-      String fooPath = "data/foo.txt";
+            String fooContents = "foo is the best";
+            String fooPath = "data/foo.txt";
 
-      String barContents = "bar is the best";
-      String barPath = "data/subdir/ignore/bar.txt";
+            String barContents = "bar is the best";
+            String barPath = "data/subdir/ignore/bar.txt";
 
-      // write zip file:
-      ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(tmp.getAbsolutePath()));
-      ZipUtil.write(zos, fooPath, ByteUtil.fromString(fooContents));
-      ZipUtil.write(zos, barPath, ByteUtil.fromString(barContents));
-      zos.close();
+            // write zip file:
+            ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(tmp.getAbsolutePath()));
+            ZipUtil.write(zos, fooPath, ByteUtil.fromString(fooContents));
+            ZipUtil.write(zos, barPath, ByteUtil.fromString(barContents));
+            zos.close();
 
-      ZipFile zipFile = ZipUtil.open(tmp);
+            ZipFile zipFile = ZipUtil.open(tmp);
 
-      // read zip file:
-      List<String> entries = ZipUtil.listZipFile(zipFile);
-      assertEquals(2, entries.size());
-      assertEquals(fooPath, entries.get(0));
-      assertEquals(barPath, entries.get(1));
+            // read zip file:
+            List<String> entries = ZipUtil.listZipFile(zipFile);
+            assertEquals(2, entries.size());
+            assertEquals(fooPath, entries.get(0));
+            assertEquals(barPath, entries.get(1));
 
-      assertEquals(fooContents, StreamUtil.copyStreamToString(ZipUtil.streamZipEntry(zipFile, fooPath)));
-      assertEquals(barContents, StreamUtil.copyStreamToString(ZipUtil.streamZipEntry(zipFile, barPath)));
+            assertEquals(fooContents, StreamUtil.copyStreamToString(ZipUtil.streamZipEntry(zipFile, fooPath)));
+            assertEquals(barContents, StreamUtil.copyStreamToString(ZipUtil.streamZipEntry(zipFile, barPath)));
 
-      assertTrue(ZipUtil.hasZipExtension(tmp.getAbsolutePath()));
+            assertTrue(ZipUtil.hasZipExtension(tmp.getAbsolutePath()));
 
-    } finally {
-      if(tmp != null) assertTrue(tmp.delete());
+        } finally {
+            if (tmp != null) {
+                tmp.delete();
+            }
+        }
+
     }
-
-  }
 }
