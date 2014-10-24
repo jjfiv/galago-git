49a575688393530e953e48faaa19c9d43baff79f
michaelz <unknown>
2014-10-24 15:38:33 -0400

    Updated so the help will be shown rather than throwing and exception if you just type "galago dump-term-stats"

--- core/src/main/java/org/lemurproject/galago/core/tools/apps/DumpTermStatisticsFn.java
+++ core/src/main/java/org/lemurproject/galago/core/tools/apps/DumpTermStatisticsFn.java
@@ -19,45 +19,51 @@ import java.io.PrintStream;
  */
 public class DumpTermStatisticsFn extends AppFunction {
 
-  @Override
-  public String getName() {
-    return "dump-term-stats";
-  }
-
-  @Override
-  public String getHelpString() {
-    return "galago dump-term-stats <index-part> \n\n"
-            + "  Dumps <term> <frequency> <document count> statsistics from the"
-            + " the specified index part.\n";
-  }
-
-  @Override
-  public void run(String[] args, PrintStream output)
-          throws Exception {
-    ScoringContext sc = new ScoringContext();
-    IndexPartReader reader = DiskIndex.openIndexPart(args[1]);
-    KeyIterator iterator = reader.getIterator();
-    while (!iterator.isDone()) {
-      CountIterator mci = (CountIterator) iterator.getValueIterator();
-      long frequency = 0;
-      long documentCount = 0;
-      while (!mci.isDone()) {
-        sc.document = mci.currentCandidate();
-        if (mci.hasMatch(mci.currentCandidate())) {
-          frequency += mci.count(sc);
-          documentCount++;
-        }
-        mci.movePast(mci.currentCandidate());
-      }
-      output.printf("%s\t%d\t%d\n", iterator.getKeyString(), frequency, documentCount);
-      iterator.nextKey();
+    @Override
+    public String getName() {
+        return "dump-term-stats";
     }
-    reader.close();
-  }
 
-  @Override
-  public void run(Parameters p, PrintStream output) throws Exception {
-    String indexPath = p.getString("indexPath");
-    run(new String[]{"", indexPath}, output);
-  }
+    @Override
+    public String getHelpString() {
+        return "galago dump-term-stats <index-part> \n\n"
+                + "  Dumps <term> <frequency> <document count> statsistics from the"
+                + " the specified index part.\n";
+    }
+
+    @Override
+    public void run(String[] args, PrintStream output)
+            throws Exception {
+
+        if (args.length <= 1) {
+            output.println(getHelpString());
+            return;
+        }
+
+        ScoringContext sc = new ScoringContext();
+        IndexPartReader reader = DiskIndex.openIndexPart(args[1]);
+        KeyIterator iterator = reader.getIterator();
+        while (!iterator.isDone()) {
+            CountIterator mci = (CountIterator) iterator.getValueIterator();
+            long frequency = 0;
+            long documentCount = 0;
+            while (!mci.isDone()) {
+                sc.document = mci.currentCandidate();
+                if (mci.hasMatch(mci.currentCandidate())) {
+                    frequency += mci.count(sc);
+                    documentCount++;
+                }
+                mci.movePast(mci.currentCandidate());
+            }
+            output.printf("%s\t%d\t%d\n", iterator.getKeyString(), frequency, documentCount);
+            iterator.nextKey();
+        }
+        reader.close();
+    }
+
+    @Override
+    public void run(Parameters p, PrintStream output) throws Exception {
+        String indexPath = p.getString("indexPath");
+        run(new String[]{"", indexPath}, output);
+    }
 }
