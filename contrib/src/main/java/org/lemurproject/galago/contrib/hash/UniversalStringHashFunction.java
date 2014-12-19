c881c42f1a39364504e117e9ce3eec94f4678310
John Foley <jfoley@cs.umass.edu>
2014-12-19 09:56:10 -0500

    Move Snowball Stemmers out of core.

--- contrib/src/main/java/org/lemurproject/galago/contrib/hash/UniversalStringHashFunction.java
+++ contrib/src/main/java/org/lemurproject/galago/contrib/hash/UniversalStringHashFunction.java
@@ -72,9 +72,9 @@ public class UniversalStringHashFunction {
     // p2 must be larger than p1 (and universe, and width)
     BigInteger p2 = p1.multiply(BigInteger.valueOf(3)).nextProbablePrime();
 
-    BigInteger a1 = BigInteger.ZERO;
-    BigInteger a2 = BigInteger.ZERO;
-    BigInteger b2 = BigInteger.ZERO;
+    BigInteger a1;
+    BigInteger a2;
+    BigInteger b2;
 
     a1 = new BigInteger(p1.bitLength(), rnd);
     while (a1.compareTo(BigInteger.ZERO) <= 0 || a1.compareTo(p1) >= 0) {
