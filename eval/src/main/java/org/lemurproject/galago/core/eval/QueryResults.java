93bfd34aff1181fa8878047e0425fd786c3b69c0
John Foley <jfoley@cs.umass.edu>
2015-02-27 16:43:43 -0500

    Add an "unknown-id" constructor.

--- eval/src/main/java/org/lemurproject/galago/core/eval/QueryResults.java
+++ eval/src/main/java/org/lemurproject/galago/core/eval/QueryResults.java
@@ -3,6 +3,7 @@
  */
 package org.lemurproject.galago.core.eval;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -15,9 +16,12 @@ public class QueryResults {
   private String query;
   private List<EvalDoc> rankedList;
 
-  public QueryResults(String query, List<EvalDoc> rankedList) {
+  public QueryResults(List<? extends EvalDoc> rankedList) {
+    this("unknown-id", rankedList);
+  }
+  public QueryResults(String query, List<? extends EvalDoc> rankedList) {
     this.query = query;
-    this.rankedList = rankedList;
+    this.rankedList = new ArrayList<>(rankedList);
   }
 
   public Iterable<EvalDoc> getIterator() {
