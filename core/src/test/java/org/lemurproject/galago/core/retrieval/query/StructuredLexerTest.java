517cf83d93b8939c45653215b16e583566cff390
Stephen Harding <harding@cs.umass.edu>
2017-11-22 11:33:55 -0500

    Added tests for behavior of queries with dots in them.

--- core/src/test/java/org/lemurproject/galago/core/retrieval/query/StructuredLexerTest.java
+++ core/src/test/java/org/lemurproject/galago/core/retrieval/query/StructuredLexerTest.java
@@ -97,4 +97,104 @@ public class StructuredLexerTest {
     assertEquals("b c d", t.text);
     assertEquals(0, t.position);
   }
+
+  @Test
+  public void testSimpleDotTokens () throws Exception {
+    //- A dot at the end of a query, or with no field specifier after
+    //  it should be tokenized without the dot.
+    StructuredLexer lexer = new StructuredLexer ();
+    List<Token> tokens = lexer.tokens ("world. ");
+    Iterator<Token> iterator = tokens.iterator();
+
+    Token t = iterator.next();
+    assertEquals("world", t.text);
+    assertEquals(0, t.position);
+
+    assertFalse(iterator.hasNext());
+  }
+
+  @Test
+  public void testStructuredDotTokens () throws Exception {
+    //- A structured query term with a field specification should tokenzie
+    //  the dot.  Note that if a dot appears at the end of such a query,
+    //  the parse should not fail, but it will still be an illegal query.
+    StructuredLexer lexer = new StructuredLexer ();
+    List<Token> tokens = lexer.tokens ("#combine(world.headline)");
+    Iterator<Token> iterator = tokens.iterator();
+
+    Token t = iterator.next();
+    assertEquals("#", t.text);
+    assertEquals(0, t.position);
+
+    t = iterator.next();
+    assertEquals("combine", t.text);
+    assertEquals(1, t.position);
+
+    t = iterator.next();
+    assertEquals("(", t.text);
+    assertEquals(8, t.position);
+
+    t = iterator.next();
+    assertEquals("world", t.text);
+    assertEquals(9, t.position);
+
+    t = iterator.next();
+    assertEquals(".", t.text);
+    assertEquals(14, t.position);
+
+    t = iterator.next();
+    assertEquals("headline", t.text);
+    assertEquals(15, t.position);
+
+    t = iterator.next();
+    assertEquals(")", t.text);
+    assertEquals(23, t.position);
+
+
+    assertFalse(iterator.hasNext());
+  }
+
+  @Test
+  public void testCombinedSimpleStructuredDotTokens () throws Exception {
+    //- A mix of simple and structure query terms should either ignore the
+    //  dot, or correctly tokenize it as a separator for a field term.
+    StructuredLexer lexer = new StructuredLexer ();
+    List<Token> tokens = lexer.tokens ("#combine(world.  world.headline)");
+    Iterator<Token> iterator = tokens.iterator();
+
+    Token t = iterator.next();
+    assertEquals("#", t.text);
+    assertEquals(0, t.position);
+
+    t = iterator.next();
+    assertEquals("combine", t.text);
+    assertEquals(1, t.position);
+
+    t = iterator.next();
+    assertEquals("(", t.text);
+    assertEquals(8, t.position);
+
+    t = iterator.next();
+    assertEquals("world", t.text);
+    assertEquals(9, t.position);
+
+    t = iterator.next();
+    assertEquals("world", t.text);
+    assertEquals(17, t.position);
+
+    t = iterator.next();
+    assertEquals(".", t.text);
+    assertEquals(22, t.position);
+
+    t = iterator.next();
+    assertEquals("headline", t.text);
+    assertEquals(23, t.position);
+
+    t = iterator.next();
+    assertEquals(")", t.text);
+    assertEquals(31, t.position);
+
+    assertFalse(iterator.hasNext());
+  }
+
 }
