5295b917f5f9e258e5d18a606e725e2ee9161a85
Gregory Brooks <glbrooks@umass.edu>
2020-12-04 16:35:47 -0500

    Update snowball and add arabic and tamil stemmers

--- snowball-stemmers/src/main/java/org/tartarus/snowball/TestApp.java
+++ snowball-stemmers/src/main/java/org/tartarus/snowball/TestApp.java
@@ -1,67 +1,74 @@
 
 package org.tartarus.snowball;
 
-import java.io.*;
+import java.lang.reflect.Method;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.Writer;
 
 public class TestApp {
-  private static void usage()
-  {
-    System.err.println("Usage: TestApp <algorithm> <input file> [-o <output file>]");
-  }
-
-  public static void main(String [] args) throws Throwable {
-    if (args.length < 2) {
-      usage();
-      return;
+    private static void usage()
+    {
+        System.err.println("Usage: TestApp <algorithm> [<input file>] [-o <output file>]");
     }
 
-    Class stemClass = Class.forName("org.tartarus.snowball.ext." +
-        args[0] + "Stemmer");
-    SnowballStemmer stemmer = (SnowballStemmer) stemClass.newInstance();
-
-    Reader reader;
-    reader = new InputStreamReader(new FileInputStream(args[1]));
-    reader = new BufferedReader(reader);
-
-    StringBuffer input = new StringBuffer();
-
-    OutputStream outstream;
-
-    if (args.length > 2) {
-      if (args.length >= 4 && args[2].equals("-o")) {
-        outstream = new FileOutputStream(args[3]);
-      } else {
-        usage();
-        return;
-      }
-    } else {
-      outstream = System.out;
-    }
-    Writer output = new OutputStreamWriter(outstream);
-    output = new BufferedWriter(output);
-
-    int repeat = 1;
-    if (args.length > 4) {
-      repeat = Integer.parseInt(args[4]);
-    }
-
-    int character;
-    while ((character = reader.read()) != -1) {
-      char ch = (char) character;
-      if (Character.isWhitespace(ch)) {
-        if (input.length() > 0) {
-          stemmer.setCurrent(input.toString());
-          for (int i = repeat; i != 0; i--) {
-            stemmer.stem();
-          }
-          output.write(stemmer.getCurrent());
-          output.write('\n');
-          input.delete(0, input.length());
+    public static void main(String [] args) throws Throwable {
+	if (args.length < 2) {
+            usage();
+            return;
         }
-      } else {
-        input.append(Character.toLowerCase(ch));
-      }
+
+	Class stemClass = Class.forName("org.tartarus.snowball.ext." +
+					args[0] + "Stemmer");
+        SnowballStemmer stemmer = (SnowballStemmer) stemClass.newInstance();
+
+	int arg = 1;
+
+	InputStream instream;
+	if (args.length > arg && !args[arg].equals("-o")) {
+	    instream = new FileInputStream(args[arg++]);
+	} else {
+	    instream = System.in;
+	}
+
+        OutputStream outstream;
+	if (args.length > arg) {
+            if (args.length != arg + 2 || !args[arg].equals("-o")) {
+                usage();
+                return;
+            }
+	    outstream = new FileOutputStream(args[arg + 1]);
+	} else {
+	    outstream = System.out;
+	}
+
+	Reader reader = new InputStreamReader(instream);
+	reader = new BufferedReader(reader);
+
+	Writer output = new OutputStreamWriter(outstream);
+	output = new BufferedWriter(output);
+
+	StringBuffer input = new StringBuffer();
+	int character;
+	while ((character = reader.read()) != -1) {
+	    char ch = (char) character;
+	    if (Character.isWhitespace(ch)) {
+		stemmer.setCurrent(input.toString());
+		stemmer.stem();
+		output.write(stemmer.getCurrent());
+		output.write('\n');
+		input.delete(0, input.length());
+	    } else {
+		input.append(ch < 127 ? Character.toLowerCase(ch) : ch);
+	    }
+	}
+	output.flush();
     }
-    output.flush();
-  }
 }
